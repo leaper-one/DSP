ef299b6f972747c3e61695459d3f15265f82b93d
tymon42 <tymon42@163.com>
2020-03-07 16:05:58 +0800

    Generate client

--- client.py
+++ client.py
@@ -1,12 +1,3 @@
-import socket
+import requests
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-data = b'aaaaaaaa'
-# 发送数据:
-s.sendto(data, ('127.0.0.1', 9998))
-# 接收数据:
-print(s.recv(1024).decode('utf-8'))
-b = b+"0"
-print(len(b))
-s.close()
+msg = 'test_msg_from_client'

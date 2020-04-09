772c4a1157dbd2a62c617cb4462a6594ea218c1c
tymon42 <tymon42@163.com>
2020-04-09 10:09:20 +0800

    :memo: add use msg

--- fundmethood.py
+++ fundmethood.py
@@ -11,13 +11,21 @@ import gzip
 import pressone_config
 from assets import CNB
 import umsgpack
+from inspect import signature
 from secretsharing import SecretSharer, PlaintextToHexSecretSharer
 '''
 生成一个uuid格式的trace
 '''
 def genTrace():
     return str(uuid.uuid1())
-
+'''
+切片四件套
+使用：
+1.将需要的字符串 data 进行 slice 得到多分切片，
+2.将每份切片 进行 pack 用以传输，
+3.将单份进行 unpack 得到切片，
+4.将多分切片进行 recover 还原 data。
+'''
 def slice(data,min,max):
     return PlaintextToHexSecretSharer.split_secret(data,min,max)
 
@@ -30,3 +38,20 @@ def pack(dic):
 def unpack(pack):#data:str
     return umsgpack.unpackb(base64.b64decode(pack))
 
+def genData(type,user_id=None,data=None,order_id=None):
+    if type == 'order':
+        r = {
+            "type": "order",
+            "user_id": user_id,
+            "order_id": order_id,
+            "data": data,
+        }
+    elif type == 'order_slice':
+        r = {
+            "type": "order_slice",
+            "user_id": user_id,
+            "order_id": order_id,
+            "data": data
+        } 
+    
+    return r

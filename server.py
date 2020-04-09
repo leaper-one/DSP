c914e1c044545465eaf16d243f50d760407731a5
tymon42 <tymon42@163.com>
2020-04-09 20:37:17 +0800

    :memo: add note

--- server.py
+++ server.py
@@ -35,60 +35,53 @@ server 用于接收 UDP 消息
 '''
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('127.0.0.1', 8899)) # 绑定端口:
+server.bind(('127.0.0.1', 8899))
 
 while True:
+    '''
+    收取 UDP 消息，变量 data 存在固定格式，具体参看 stander_form.md 
+    '''
     data, addr = server.recvfrom(1024)
     data = fm.unpack(data)
-    data = json.loads(data.decode('utf-8'))
-    if data.get('type') != None:
-        if data.get('type') == 'order':
+    data = json.loads(data.decode('utf-8'))#data 为 dic
+
+    if data.get('type') != None:#
+        '''
+        订单：接收到订单，查询是否存在于本地，若否，切片，广播分发
+        '''
+        if data['type'] == 'order':
             order_stored = Query()
             r = order_db.search(order_stored.order_id == data['order_id'])
+
             if r == []:
-                print('收到 '+data.get('id')+' 订单')
-                #TODO: 计算验证消息、储存订单、打包msgpack、切片并生成列表
-                order_slice_org = {
-                    "type": "order",
-                    "user_id": data['user_id'],
-                    "order_id": data['order_id'],
-                    "data": {#TODO:需更改
-                        "file_type": "mine",#TODO:获取文件类型
-                        "file_hash": "str(md5)",
-                        "file_slices_hash": "list"
-                    },
-                    "date":"str",
-                    "order_hash":"str"
-                }#仅为测试格式,需要转换成 stander_form.md 中的标准格式
-                order_db.insert(order_slice_org)
-                order_slice_list = fm.slice(order_slice_org,len(DS_list)*2//3+1,len(DS_list))
-                print(order_slice_list)
+                #TODO: 验证订单内容
+                order_slice_org = fm.genData('order',data['user_id'],data['data'],order_id=data['order_id'])
+                order_db.insert(order_slice_org)#储存订单
+                order_slice_list = fm.slice(order_slice_org,len(DS_list)*2//3+1,len(DS_list))#打包msgpack
+                
                 for i in range(len(DS_list)*2):#向其他云节点广播两轮 order_slice 信息
                     for osl in order_slice_list:
-                        data = {
-                            "type": "order_slice",
-                            "user_id":order_slice_org["user_id"],
-                            "order_id": order_slice_org["order_id"],
-                            "order_hash":order_slice_org["order_hash"],
-                            "data": osl
-                        }
-                        random_DS = random.choice(DS_list)
-                        client.sendto(fm.pack(data), (random_DS, 8899))# 广播数据:
-                        client.close()
-            else:
-                pass
-            
+                        data = fm.genData("order_slice",user_id=order_slice_org["user_id"],order_id=order_slice_org["order_id"],data=[osl])
+                        data = fm.pack(data)
+                        client.sendto(data, (random.choice(DS_list),8899))# 广播数据
+        '''
+        订单切片
+        '''
         if data.get('type') == 'order_slice':
-            order_id = data['order_id']
-            print('收到 '+order_id+' 订单切片')
             Slice = Query()#查询相同 order_id 的切片文件
-            r = order_slice_db.search(Slice.order_id)
+            r = order_slice_db.search(Slice.data['order_id'])
             if r == []:
-                print('是你没玩过的船新版本')
                 order_slice_db.insert(data)
+                for i in range(len(DS_list)//2):
+                    client.sendto(fm.pack(data),(random.choice(DS_list), 8899))#再广播:
+                    client.close()
             else:
-                print('又来一份')
                 order_slice_db.insert(data)
+                Slice = Query()#查询相同 order_id 的切片文件
+                r = order_slice_db.search(Slice.data['order_id'])
+                data = fm.genData('order_slice',user_id=data['user_id'],data=r)
+                client.sendto(fm.pack(data), (random.choice(DS_list), 8899))#再广播:
+                client.close()
             a = list(set(order_slice_db.search(Slice.order_id)))#去重
             if len(a) > len(DS_list)*2//3+1:#判断 BFT 结果，并还原
                 a = fm.recover(a)
